9339bd1b8bae7a2a7a3357ac09095c1e5a9883b0
SysError99 <17522480+SysError99@users.noreply.github.com>
2022-12-25 05:02:24 +0700

    Test cache file path map#2

--- src/func/file.ts
+++ src/func/file.ts
@@ -3,12 +3,62 @@ import { encode as base64urlEncode } from "https://deno.land/std@0.170.0/encodin
 import { decode as msgpackDecode, encode as msgpackEncode } from "https://esm.sh/@msgpack/msgpack@2.8.0";
 
 
-const pathMap = new Map();
+const pathMap1 = new Map();
+const pathMap2 = new Map();
+const pathMap3 = new Map();
+const pathMap4 = new Map();
+const pathMap5 = new Map();
+const pathMap6 = new Map();
+const pathMap7 = new Map();
+const pathMap8 = new Map();
+const maxPathMap = 1000000;
+const maxPathMapPart = Math.floor(maxPathMap / 8);
+const pathMaps = [
+    pathMap1,
+    pathMap2,
+    pathMap3,
+    pathMap4,
+    pathMap5,
+    pathMap6,
+    pathMap7,
+    pathMap8,
+];
+const pathMapCounts = [
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+]
 
 
 async function convertToFilePath(urlPathname: string): Promise<string> {
-    if (pathMap.has(urlPathname)) {
-        return pathMap.get(urlPathname);
+    if (pathMap1.has(urlPathname)) {
+        return pathMap1.get(urlPathname);
+    }
+    if (pathMap2.has(urlPathname)) {
+        return pathMap2.get(urlPathname);
+    }
+    if (pathMap3.has(urlPathname)) {
+        return pathMap3.get(urlPathname);
+    }
+    if (pathMap4.has(urlPathname)) {
+        return pathMap4.get(urlPathname);
+    }
+    if (pathMap5.has(urlPathname)) {
+        return pathMap5.get(urlPathname);
+    }
+    if (pathMap6.has(urlPathname)) {
+        return pathMap6.get(urlPathname);
+    }
+    if (pathMap7.has(urlPathname)) {
+        return pathMap7.get(urlPathname);
+    }
+    if (pathMap8.has(urlPathname)) {
+        return pathMap8.get(urlPathname);
     }
     const p = "./data/" + base64urlEncode(
         await crypto.subtle.digest(
@@ -16,7 +66,17 @@ async function convertToFilePath(urlPathname: string): Promise<string> {
             new TextEncoder().encode(urlPathname),
         )
     ) + ".bin";
-    pathMap.set(urlPathname, p);
+    while (true) {
+        const rand = Math.floor(Math.random() * pathMaps.length);
+        const pathMap = pathMaps[rand]
+        if (pathMapCounts[rand] > maxPathMapPart ) {
+            pathMapCounts[rand] = 0;
+            pathMap.clear();
+        }
+        pathMap.set(urlPathname, p);
+        pathMapCounts[rand]++;
+        break;
+    }
     return p;
 }
 
