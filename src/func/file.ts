01d50a43071f0031500f527f9c49406bb0ec6005
SysError99 <17522480+SysError99@users.noreply.github.com>
2022-12-25 20:23:43 +0700

    Use XX64 hash.

--- src/func/file.ts
+++ src/func/file.ts
@@ -1,6 +1,8 @@
-import { crypto } from "https://deno.land/std@0.170.0/crypto/mod.ts";
-import { encode as base64urlEncode } from "https://deno.land/std@0.170.0/encoding/base64url.ts";
 import { decode as msgpackDecode, encode as msgpackEncode } from "https://esm.sh/@msgpack/msgpack@2.8.0";
+import { create as XXH64Create } from "https://deno.land/x/xxhash64@1.0.0/mod.ts";
+
+
+const h = await XXH64Create();
 
 
 function later(delay: number): Promise<void> {
@@ -10,68 +12,19 @@ function later(delay: number): Promise<void> {
 }
 
 
-const cachedPath1 = new Map();
-const cachedPath2 = new Map();
-const cachedPath3 = new Map();
-const cachedPath4 = new Map();
-const maxCacheMap = 131072;
-const maxCacheMapPart = Math.floor(maxCacheMap / 4);
-const cachedPaths = [
-    cachedPath1,
-    cachedPath2,
-    cachedPath3,
-    cachedPath4,
-];
-const cachedPathCounts = [
-    0,
-    0,
-    0,
-    0,
-]
-
-
-async function convertToFilePath(urlPathname: string): Promise<string> {
-    if (cachedPath1.has(urlPathname)) {
-        return cachedPath1.get(urlPathname);
-    }
-    if (cachedPath2.has(urlPathname)) {
-        return cachedPath2.get(urlPathname);
-    }
-    if (cachedPath3.has(urlPathname)) {
-        return cachedPath3.get(urlPathname);
-    }
-    if (cachedPath4.has(urlPathname)) {
-        return cachedPath4.get(urlPathname);
-    }
-    const p = "./data/" + base64urlEncode(
-        await crypto.subtle.digest(
-            'SHA-256',
-            new TextEncoder().encode(urlPathname),
-        )
-    ) + ".bin";
-    while (true) {
-        const rand = Math.floor(Math.random() * cachedPaths.length);
-        const cachedPath = cachedPaths[rand]
-        if (cachedPathCounts[rand] > maxCacheMapPart) {
-            cachedPathCounts[rand] = 0;
-            cachedPath.clear();
-        }
-        cachedPath.set(urlPathname, p);
-        cachedPathCounts[rand]++;
-        break;
-    }
-    return p;
+function convertToFilePath(urlPathname: string): string {
+    return "./data/" + h.update(urlPathname).digest('hex') + ".bin";
 }
 
 
 export async function deleteFile(urlPathname: string): Promise<void> {
-    await Deno.remove(await convertToFilePath(urlPathname));
+    await Deno.remove(convertToFilePath(urlPathname));
 }
 
 
 export async function fileExists(urlPathname: string): Promise<boolean> {
     try {
-        await Deno.stat(await convertToFilePath(urlPathname));
+        await Deno.stat(convertToFilePath(urlPathname));
         return true;
     } catch (e) {
         if (e instanceof Deno.errors.NotFound) {
@@ -86,7 +39,7 @@ export async function readFile(urlPathname: string): Promise<unknown> {
     try {
         return msgpackDecode(
             await Deno.readFile(
-                await convertToFilePath(urlPathname),
+                convertToFilePath(urlPathname),
             ),
         );
     } catch (e) {
@@ -101,7 +54,7 @@ export async function readFile(urlPathname: string): Promise<unknown> {
 
 export async function writeFile(urlPathname: string, object: unknown, createIfNotExist = true, preventOverwrite = false): Promise<void> {
     await Deno.writeFile(
-        await convertToFilePath(urlPathname),
+        convertToFilePath(urlPathname),
         msgpackEncode(object),
         {
             create: createIfNotExist,
